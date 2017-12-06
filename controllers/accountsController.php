cb04d948b2b35f1d44602a6c3a09394dc9454f01
np549 <np549@njit.edu>
2017-12-05 23:57:01 -0500

    accoutns controller

--- controllers/accountsController.php
+++ controllers/accountsController.php
@@ -5,12 +5,9 @@
  * Date: 11/27/17
  * Time: 5:32 PM
  */
-
-
 //each page extends controller and the index.php?page=tasks causes the controller to be called
 class accountsController extends http\controller
 {
-
     //each method in the controller is named an action.
     //to call the show function the url is index.php?page=task&action=show
     public static function show()
@@ -18,66 +15,64 @@ class accountsController extends http\controller
         $record = accounts::findOne($_REQUEST['id']);
         self::getTemplate('show_account', $record);
     }
-
     //to call the show function the url is index.php?page=accounts&action=all
-
     public static function all()
     {
-
         $records = accounts::findAll();
         self::getTemplate('all_accounts', $records);
-
     }
     //to call the show function the url is called with a post to: index.php?page=task&action=create
     //this is a function to create new tasks
-
     //you should check the notes on the project posted in moodle for how to use active record here
-
     //this is to register an account i.e. insert a new account
     public static function register()
     {
         //https://www.sitepoint.com/why-you-should-use-bcrypt-to-hash-stored-passwords/
         //USE THE ABOVE TO SEE HOW TO USE Bcrypt
-      self::getTemplate('register');
-        
+        self::getTemplate('register');
     }
-//this is the function to save the user the new user for registration
-    //this is the function to save the user the user profile
+    //this is the function to save the user the new user for registration
     public static function store()
     {
-         $user = accounts::findUserbyEmail($_REQUEST['email']);
- +
- +
- +        if ($user == FALSE) {
- +            $user = new account();
- +            $user->email = $_POST['email'];
- +            $user->fname = $_POST['fname'];
- +            $user->lname = $_POST['lname'];
- +            $user->phone = $_POST['phone'];
- +            $user->birthday = $_POST['birthday'];
- +            $user->gender = $_POST['gender'];
- +            //$user->password = $_POST['password'];
- +            $user->password = $user->setPassword($_POST['password']);
- +            $user->save();
- +            header("Location: index.php?page=accounts&action=all");
- +
- +        } else {
- +
- +            echo 'already registered';
- +        }
-
+        $user = accounts::findUserbyEmail($_REQUEST['email']);
+        if ($user == FALSE) {
+            $user = new account();
+            $user->email = $_POST['email'];
+            $user->fname = $_POST['fname'];
+            $user->lname = $_POST['lname'];
+            $user->phone = $_POST['phone'];
+            $user->birthday = $_POST['birthday'];
+            $user->gender = $_POST['gender'];
+            //$user->password = $_POST['password'];
+            $user->password = $user->setPassword($_POST['password']);
+            $user->save();
+            header("Location: index.php?page=accounts&action=all");
+        } else {
+            echo 'already registered';
+        }
     }
-self::getTemplate('edit_account', $record);
-
-}
     public static function edit()
     {
         $record = accounts::findOne($_REQUEST['id']);
-
         self::getTemplate('edit_account', $record);
-
     }
-
+//this is used to save the update form data
+    public static function save() {
+        $user = accounts::findOne($_REQUEST['id']);
+        $user->email = $_POST['email'];
+        $user->fname = $_POST['fname'];
+        $user->lname = $_POST['lname'];
+        $user->phone = $_POST['phone'];
+        $user->birthday = $_POST['birthday'];
+        $user->gender = $_POST['gender'];
+        $user->save();
+        header("Location: index.php?page=accounts&action=all");
+    }
+    public static function delete() {
+        $record = accounts::findOne($_REQUEST['id']);
+        $record->delete();
+        header("Location: index.php?page=accounts&action=all");
+    }
     //this is to login, here is where you find the account and allow login or deny.
     public static function login()
     {
@@ -86,10 +81,19 @@ self::getTemplate('edit_account', $record);
         //then you need to check the password and create the session if the password matches.
         //you might want to add something that handles if the password is invalid, you could add a page template and direct to that
         //after you login you can use the header function to forward the user to a page that displays their tasks.
-         //   $record = accounts::findUser($_POST['uname']);
-
-        print_r($_POST);
-
+        //        $record = accounts::findUser($_POST['uname']);
+        $user = accounts::findUserbyEmail($_REQUEST['email']);
+        if ($user == FALSE) {
+            echo 'user not found';
+        } else {
+            if($user->checkPassword($_POST['password']) == TRUE) {
+                echo 'login';
+                session_start();
+                $_SESSION["userID"] = $user->id;
+                print_r($_SESSION);
+            } else {
+                echo 'password does not match';
+            }
+        }
     }
-
 }
