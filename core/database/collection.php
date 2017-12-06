b184eaff72e0b594f85c97f6c8bb439baed0aab3
np549 <np549@njit.edu>
2017-12-06 00:00:06 -0500

    database edit

--- core/database/collection.php
+++ core/database/collection.php
@@ -20,7 +20,13 @@ abstract class collection
     }
 
     //you can use this to run other queries in on classes that extend the collection class because this is protected
-    protected static function getResults($sql) {
+     protected static function getResults($sql, $parameters = null) {
+ +
+ +
+ +        if (!is_array($parameters)) {
+ +            $parameters = (array) $parameters;
+ +
+ +        }
         $db = dbConn::getConnection();
         $statement = $db->prepare($sql);
         $statement->execute();
