5295d676b6b1a6d223502538ee9afe1190c2a90e
np549 <np549@njit.edu>
2017-12-16 23:08:30 -0500

    not found pages changed

--- pages/notfound.php
+++ pages/notfound.php
@@ -9,6 +9,7 @@
     <meta name="author" content="SitePoint">
 
     <link rel="stylesheet" href="css/styles.css?v=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
 
     <!--[if lt IE 9]>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
